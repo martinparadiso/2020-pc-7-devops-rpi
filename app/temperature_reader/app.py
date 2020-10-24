625a557f7eb5d3d2690091ac9ac4f3b6b150d5de
Martín Paradiso <martinparadiso@outlook.com>
2020-10-23 21:11:04 -0300

    Lectura del sensor + web

---
+++ app/temperature_reader/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask import jsonify
+
+import sensors.temperature
+
+app = Flask(__name__)
+
+@app.route('/')
+def sensor():
+    ret = {
+        "temperature" : sensors.temperature.get_temperature()
+    }
+    return jsonify(ret)
+
+if __name__ == '__main__':
+    app.run()
---
+++ app/temperature_reader/sensors/temperature.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1115(i2c)
+
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P0)
+
+# Constant to change from voltage to celsius
+c = 100
+
+def get_temperature():
+    voltage = chan.voltage
+    return round(voltage * c, 3)
